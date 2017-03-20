ad423fc01eecd9e80e17339a33f98679fb41c115
Dennis <dennis.djp@gmail.com>
2017-03-19 22:54:33 -0500

    number

--- number.go
+++ number.go
@@ -1,1 +1,20 @@
-number.go
+// # +  x y z
+// # +  x y z
+// # +  x y z
+// #    -----
+// #    y y y
+// #    ----- 
+// What is the value of x y z
+
+package main
+
+import fmt
+
+func main() {
+	var int x 
+	var int y
+	var int z
+	for {
+		x := 1
+	}
+}
